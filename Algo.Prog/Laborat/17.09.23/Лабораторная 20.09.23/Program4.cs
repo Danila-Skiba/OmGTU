19ddb36e2054ef80d96ac4cf179fe63afa47bcad
Danila-Skiba <144258867+Danila-Skiba@users.noreply.github.com>
2023-10-01 14:56:48 +0600

    Add files via upload

---
+++ Algo.Prog/Laborat/17.09.23/Лабораторная 20.09.23/Program4.cs
@@ -0,0 +1,46 @@
+﻿//4) Определить образует ли заданная последовательность равномерно убывающую последовательность элементов
+using System;
+
+namespace Laborator_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int pred = int.Parse(Console.ReadLine()); 
+            bool flag = true;
+            int step = 0;
+            for (int i = 1; i < num; i++)
+            {
+                int now = int.Parse(Console.ReadLine());
+                if (i == 1) 
+                {
+                    step = pred - now;
+                }
+                if (i != 1)
+                {
+                    if (pred- now == step)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+                pred = now; 
+            }
+                if (flag == false)
+                {
+                    Console.WriteLine("не равномерно убывающая последовательность");
+                }
+                else
+                {
+                    Console.WriteLine("равномерно убывающая последовательность");
+                }
+            }
+        }
+    }
+
+
