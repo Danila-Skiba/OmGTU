a11839151576c431706b1e96913bd66bbbd7121c
Danila-Skiba <144258867+Danila-Skiba@users.noreply.github.com>
2023-10-01 22:29:13 +0600

    Add files via upload

---
+++ Algo.Prog/Laborat/17.09.23/Лабораторная 19.09.23/Programlab1.cs
@@ -0,0 +1,54 @@
+﻿
+/*Дана последовательность элемент в количестве 10 и для этой последовательности найти:
+•  Максимальный элемент
+•  Количество отрицательных
+•  Сумму положительных кратных 3
+•  Минимальный среди положительных
+*/
+using System;
+
+namespace Laborator_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность из 10 чисел");
+            int sum = 0;
+            int min_p = -1;
+            int count = 0;
+            int max = int.Parse(Console.ReadLine());
+            if (max > 0)
+                min_p = max;
+            if (max < 0)
+                count++;
+            if (max > 0 && max % 3 == 0)
+                sum++;
+            for (int i = 1; i < 10; i++)
+            {
+                int f = int.Parse(Console.ReadLine());
+                if (f > max)
+                    max = f;
+                if (f >= 0 && min_p <=0)
+                    min_p = f;
+                if (f <= min_p && f >= 0)
+                    min_p = f;
+                if ((f % 3) == 0 && f > 0)
+                    sum += f;
+                if (f < 0)
+                    count++;
+            }
+            Console.WriteLine("1) Максимальное число: " + max);
+            Console.WriteLine("2) Количество отрицательных: " + count);
+            Console.WriteLine("3) Сумма положительных чисел, кратных 3: " + sum);
+            if (min_p <=0 )
+                Console.WriteLine("4) Минимальное положительное число не найдено");
+            else
+                Console.WriteLine("4) Минимальное положительное число: " + min_p);
+        }
+    }
+}
+
+
+
+
---
+++ Algo.Prog/Laborat/17.09.23/Лабораторная 19.09.23/Programlab2.cs
@@ -0,0 +1,70 @@
+﻿
+/*Дана последовательность из n элементов:
+•  Определить кол-во элементов, значения которых больше предшествующего соседа
+•  Определить кол-во элементов, значения которых меньше всех вводимых до него
+•  Определить кол-во элементов, значения которых больше левого и правого соседа
+•  Определить наибольшую длину возрастающей подпоследовательности
+*/
+using System;
+
+namespace Laborator_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов последовательности");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            int count_2 = 0;
+            int count_3 = 0;
+            int len = 1;
+            int count_4 = 1;
+            int min;
+            Console.WriteLine("Введите последовательность из " + n + " элементов");
+            int l = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            min = l;
+            if (k > l)
+            {
+                count++;
+                len++;
+            }
+            if (k < l)
+            {
+                count_2++;
+                min = k;
+            }
+            if (len > count_4)
+                count_4 = l;
+            for (int i = 2; i < n; i++)
+            {
+                int f = int.Parse(Console.ReadLine());
+                if (f > k)
+                {
+                    count++;
+                    len++;
+                }
+                else
+                    len = 1;
+                if (f < min)
+                {
+                    count_2++;
+                    min = f;
+                }
+                if (k > l && k > f)
+                {
+                    count_3++;
+                }
+                if (len > count_4)
+                    count_4 = len;
+                l = k;
+                k = f;
+            }
+            Console.WriteLine("1) Количество элементов, значения которых больше предшествующего соседа: " + count);
+            Console.WriteLine("2) Количество элементов, значения которых меньше всех вводимых до него:  " + count_2);
+            Console.WriteLine("3) Количество элементов, значения которых больше левого и правого соседа: " + count_3);
+            Console.WriteLine("4) Наибольшая длина возрастающей последовательности: " + count_4);
+        }
+    }
+}
