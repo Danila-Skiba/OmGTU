d0e352d4ed0871e9c4655fbb6b31c6c0e586288f
Danila-Skiba <144258867+Danila-Skiba@users.noreply.github.com>
2023-10-26 00:11:30 +0600

    Add files via upload

---
+++ Algo.Prog/Laborat/17.09.23/Лабораторная 1-4 до 29.10.23/Лаб3 В-20(зад.1); В-7(зад.2)/ProgramLab3z2.cs
@@ -0,0 +1,16 @@
+﻿float n = 20;
+float a = -1;
+float b = 3;
+float c = n / (b - a);
+float d = 1 / c;
+for (float x = -1; x <= 3; x += 0.2f)
+{
+    float F1 = (float)(Math.Pow(2, (-x)) / 100);
+    Console.WriteLine(F1);
+
+}
+for (float x = a; x <= b; x += d)
+{
+    float F2 = (float)(20 / (1 + Math.Pow(x, 2)));
+    Console.WriteLine(F2);
+}
